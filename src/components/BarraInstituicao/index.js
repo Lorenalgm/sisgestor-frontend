541d2ca8a8f384e32c99618eabf24b3e2752ecd3
Lorena <lorena@proesc.com>
2022-02-17 22:59:43 -0300

    Ajustadas variáveis

--- src/components/BarraInstituicao/index.js
+++ src/components/BarraInstituicao/index.js
@@ -5,10 +5,10 @@ import './styles.css';
 export default function BarraInstituicao(props){
     return(
         <div className="barra-instituicao-container">
-            <Link className={props.ativo == 'inicial'?'ativo opcao esquerda': 'opcao esquerda'} to="/matrizes_orcamentarias_instituicoes">Inicial</Link>
-            <Link className={props.ativo == 'fontes'?'ativo opcao': 'opcao'} to="/fontes_instituicoes">Fontes</Link>
-            <Link className={props.ativo == 'acoes'?'ativo opcao': 'opcao'} to="/acoes_instituicoes">Ações</Link>
-            <Link className={props.ativo == 'distribuicao'?'ativo opcao direita': 'opcao direita'} to="/login">Distribuição</Link>
+            <Link className={props.ativo === 'inicial'?'ativo opcao esquerda': 'opcao esquerda'} to="/matrizes_orcamentarias_instituicoes">Inicial</Link>
+            <Link className={props.ativo === 'fontes'?'ativo opcao': 'opcao'} to="/fontes_instituicoes">Fontes</Link>
+            <Link className={props.ativo === 'acoes'?'ativo opcao': 'opcao'} to="/acoes_instituicoes">Ações</Link>
+            <Link className={props.ativo === 'distribuicao'?'ativo opcao direita': 'opcao direita'} to="/login">Distribuição</Link>
         </div>
     )
 }
